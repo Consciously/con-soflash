b1d83384345fff8849e04cee3b752d68ad03ef7d
Stefan Ihle <stefanihle@gmx.net>
2023-12-19 11:03:45 +0100

    clean up branch

--- backend/qtools/qarr.test.ts
+++
@@ -1,9 +0,0 @@
-import {shuffle} from "./qarr";
-
-describe('shuffle()', () => {
-	it("it shuffles the array", () => {
-		const colors = ['red', 'blue', 'green'];
-		shuffle(colors);
-		expect(colors).toEqual(expect.arrayContaining(colors));
-	});
-});
